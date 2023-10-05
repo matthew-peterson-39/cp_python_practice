37c57ededa9cced2904e2772e1ad62d3f4486a24
Matthew Peterson <matthew.s.peterson.39@gmail.com>
2023-10-05 15:49:05 -0400

    Created os_example() method.

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
+import os
+
 # lamda example
 def lambda_example():
     say_hi = lambda name : f'Hello {name}'
     print(say_hi('matt'))
 
-    
+# Working with files
+def os_example():
+    cwd = os.getcwd() # returns the current working directory
+    print(cwd)
+
+
