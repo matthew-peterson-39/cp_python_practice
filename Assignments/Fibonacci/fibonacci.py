190af4280c1882a3e26abfd7dded5516e8b6c21f
Matthew Peterson <matthew.s.peterson.39@gmail.com>
2023-10-05 17:22:46 -0400

    fibonacci solution.

--- Assignments/Fibonacci/fibonacci.py
+++ Assignments/Fibonacci/fibonacci.py
@@ -1,2 +1,18 @@
-def fibonacci():
-    pass
+# Takes a number arguement and returns the fibonacci value of the number arguement index of the sequence.
+def fibonacci(numArg):
+    if numArg < 2:
+        if numArg == 1 or numArg == 2:
+            return 1
+        elif numArg == 0:
+            return 0
+        else:
+             return "Error. Must be a positive number."
+    else:
+        return (fibonacci(numArg-1) + fibonacci(numArg-2))
+
+print(fibonacci(-1)) # ERROR.
+print(fibonacci(0)) # 0
+print(fibonacci(1)) # 1
+print(fibonacci(2)) # 1
+print(fibonacci(6)) # 8
+print(fibonacci(7)) # 13
