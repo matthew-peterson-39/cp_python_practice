af95545570d4c13aeb00a2ee05548697e22b48f7
Matthew Peterson <matthew.s.peterson.39@gmail.com>
2023-10-05 14:52:57 -0400

    Solved factorial.

--- Assignments/Factorial/factorial.py
+++ Assignments/Factorial/factorial.py
@@ -1,2 +1,9 @@
-def factorial():
-    pass
+def factorial(numArg):
+    if numArg < 2:
+        if numArg == 1 or numArg == 0:
+            return 1
+        else:
+            return "Error. Must be a positive number."
+    else:
+        numArg *= factorial(numArg-1)
+    return numArg
