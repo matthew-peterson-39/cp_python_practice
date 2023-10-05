716465330bc2b332624a4c6b9a662f92a44bb66c
Matthew Peterson <matthew.s.peterson.39@gmail.com>
2023-10-05 14:40:30 -0400

    Factorial solution.

--- Assignments/Factorial/factorial.py
+++ Assignments/Factorial/factorial.py
@@ -1,2 +1,14 @@
-def factorial():
-    pass
+def factorial(numArg):
+    if numArg < 2:
+        if numArg == 0 or numArg == 1:
+            return 1
+        else:
+            return "Cannot be negative number."
+    else:
+        numArg *= factorial(numArg-1)
+
+    return numArg
+    
+print(factorial(3)) # 6
+print(factorial(10)) # 3628800
+print(factorial(6)) # 720
