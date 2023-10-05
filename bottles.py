6afeb0fd462dff36ef96effdf466d7f55cc5fef9
Matthew Peterson <matthew.s.peterson.39@gmail.com>
2023-10-05 11:34:39 -0400

    Psuedo code added.

--- bottles.py
+++ bottles.py
@@ -1,5 +1,20 @@
-def bottle_song():
-	print('testing')
-	return
+# psuedo
 
-bottle_song()
+'''
+	for each number in the range(start, stop, inc/dec amount)
+		check current amount against special cases
+		if current is greater than the first special case...
+			print regular prompt
+		else 
+			if current is 2 bottles
+				change second half of verse to be singular (bottles)  
+			else if current is 1 bottle
+			 	next verse changes to no more bottles of beer on the wall...
+			else
+				error?
+'''
+
+def bottle_song():
+	for i in range(99, 0, -1):
+		pass
+	return
