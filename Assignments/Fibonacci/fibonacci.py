5a30f6cebe987f09d649658fe1c2c367284a9e70
Matthew Peterson <matthew.s.peterson.39@gmail.com>
2023-10-05 20:35:33 -0400

    For_loop() working.

--- Assignments/Fibonacci/fibonacci.py
+++ Assignments/Fibonacci/fibonacci.py
@@ -1,17 +1,67 @@
-def fibonacci(numArg):
-    if numArg < 2:
-        if numArg == 1 or numArg == 2:
+# # Recursive Solution.
+# def fibonacci(numArg):
+#     # Performance diff? -> numArg < 2 
+#     # (still runs correctly but the f(2) = 1 comes from the outer else statement not the initial check)
+#     if numArg <= 2: 
+#         if numArg == 1 or numArg == 2:
+#             return 1
+#         elif numArg == 0:
+#             return 0
+#         else:
+#              return "Error. Must be a positive number."
+#     else:
+#         return (fibonacci(numArg-1) + fibonacci(numArg-2))
+    
+# While - For Loop Solutions. 
+def fibonacci(numArg, loop_type='FOR'): # TODO remove default val
+    # Check numArg initially incase it is 0 or 1
+    if numArg <= 2:
+        if numArg in {1,2}:
             return 1
         elif numArg == 0:
             return 0
         else:
-             return "Error. Must be a positive number."
+            return "Error. Negative number."
     else:
-        return (fibonacci(numArg-1) + fibonacci(numArg-2))
+        if loop_type.upper() == 'FOR':
+            return for_loop(numArg)
+        else:
+            return while_loop(numArg)
 
-print(fibonacci(-1)) # ERROR.
-print(fibonacci(0)) # 0
-print(fibonacci(1)) # 1
-print(fibonacci(2)) # 1
-print(fibonacci(6)) # 8
-print(fibonacci(7)) # 13
+def for_loop(numArg):
+   
+    # Initial Variables
+    first_in_series = 0 # first number
+    second_in_series =  1   # second number in fib series
+    current_result = 2
+    
+    for i in range(1, numArg, 1):
+        # update result
+        current_result = first_in_series + second_in_series   # NOTE -> bug if wrong variable
+        # update variables
+        first_in_series, second_in_series = second_in_series, current_result
+    
+    return f'For Loop: {current_result}'
+
+def while_loop(numArg):
+    if numArg <= 2:
+        return 1 if numArg in {1,2} else 0  # doesnt work for negative numbers yet
+    else:
+        first_in_series = 0     # first number
+        second_in_series =  1   # second number in fib series
+
+        while numArg > 1 :
+            # update result
+            current_result = first_in_series + second_in_series   # NOTE -> bug if wrong variable
+             # update variables
+            first_in_series, second_in_series = second_in_series, current_result
+            numArg -= 1
+        
+        return current_result
+        
+print(fibonacci(-1, 'FOR')) # ERROR.
+print(fibonacci(0, 'WHILE')) # 0
+print(fibonacci(1, 'WHILE')) # 1
+print(fibonacci(2, 'WHILE')) # 1
+print(fibonacci(6, 'FOR')) # 8
+print(fibonacci(7, 'WHILE')) # 13
